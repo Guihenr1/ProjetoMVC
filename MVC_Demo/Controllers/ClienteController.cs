5f359a6d07b54b49bfb5148e32271f6ca10bd7a0
Guilherme Henrique <guilherme.pomp@hotmail.com>
2018-08-27 15:10:52 -0300

    Aula 08

---
+++ MVC_Demo/Controllers/ClienteController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Demo.Models;
+
+namespace MVC_Demo.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Detalhe()
+        {
+            Cliente cliente = new Cliente()
+            {
+                ClienteId = 2,
+                Nome = "Guilherme",
+                Email = "guilherme.pomp@gmail.com",
+                Telefone = "98130-7601"
+            };
+
+            return View(cliente);
+        }
+
+        [HttpPost]
+        public ActionResult SalvarCliente(Cliente cli)
+        {
+            return View(cli);
+        }
+    }
+}
---
+++ MVC_Demo/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["Paises"] = new List<string>()
+            {
+                "Brasil",
+                "Argentina",
+                "Paraguai"
+            };
+            return View();
+        }
+
+        public string Opa()
+        {
+            return "Venho do Opa do Index!";
+        }
+
+        public string Saudacao(int id)
+        {
+            return "Saudação de número " + id;
+        }
+    }
+}
